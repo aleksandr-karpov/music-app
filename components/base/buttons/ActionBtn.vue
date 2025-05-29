63ee7a2a3c60076d38af6d0acf38bb97b0b974dd
Aleksandr <alexandr.karpov.work@gmail.com>
2025-05-29 12:04:46 +0300

    feat(buttons): add action button
    - Created ActionBtn component

---
+++ components/base/buttons/ActionBtn.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const { label = 'Action Btn', disabled = false } = defineProps<{
+  label?: string;
+  disabled?: boolean;
+}>()
+</script>
+
+<template>
+  <button
+      :disabled="disabled"
+      :class="['action-btn', { 'action-btn--disabled': disabled }]"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<style scoped lang="scss">
+@use "assets/styles/variables" as *;
+
+.action-btn {
+  height: 40px;
+  border-radius: $border-radius;
+  padding: 8px 16px;
+  background-color: $color-bg-dark;
+  color: $color-text-dark-bg;
+
+  &:hover:not(:disabled) {
+    opacity: 0.8;
+    cursor: pointer;
+  }
+
+  &--disabled {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+}
+</style>
