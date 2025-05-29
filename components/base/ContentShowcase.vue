297caed3a071025afb8ef4405523077e9e5dfcbc
Aleksandr <alexandr.karpov.work@gmail.com>
2025-05-29 18:54:50 +0300

    feat(components): add content showcase
    - Created ContentShowcase component that can display special section with title and subheading

---
+++ components/base/ContentShowcase.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const { title = 'Title', subheading = 'Subheading' } = defineProps<{
+  title: string;
+  subheading: string;
+}>();
+</script>
+
+<template>
+  <section class="content-showcase">
+    <div class="content-showcase__description">
+      <h3 class="content-showcase__title">{{ title }}</h3>
+      <h4 class="content-showcase__subheading">{{ subheading }}</h4>
+    </div>
+    <div class="content-showcase__content">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+@use "assets/styles/variables" as *;
+
+.content-showcase {
+  gap: 32px;
+
+  &, &__description {
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__description {
+    gap: 8px;
+  }
+
+  &__title {
+    color: $color-text-light-bg;
+    font-size: $font-size-title-lg;
+    font-weight: $font-weight;
+    line-height: 110%;
+  }
+
+  &__subheading {
+    color: #454545;
+    font-size: $font-size-base;
+    line-height: $line-height;
+    font-weight: normal;
+  }
+}
+</style>
