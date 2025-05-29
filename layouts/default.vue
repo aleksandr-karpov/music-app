4d2d130429535aac7fccee85dbee902662e2a5e4
Aleksandr <alexandr.karpov.work@gmail.com>
2025-05-29 10:14:15 +0300

    feat(layouts): create new default layout
    - Added base layout structure with aside and main content block
    - Applied template to display pages

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="default-layout">
+    <aside class="default-layout__sidebar"></aside>
+    <main class="default-layout__main-view vertical-scroll-area">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.default-layout {
+  display: flex;
+  height: 100%;
+
+  &__sidebar {
+    width: 255px;
+  }
+
+  &__main-view {
+    flex: 1;
+    height: 100%;
+  }
+}
+</style>
